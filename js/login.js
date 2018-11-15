04a1d004a46672271926eac355c33b232ff4f5e2
ZhouRunCheng <1721730305@qq.com>
2018-11-15 15:38:00 +0800

    登录界面的大致结构

---
+++ js/login.js
@@ -0,0 +1,44 @@
+var time = $('#timedown');
+var times = document.getElementById('s');
+var login = $('#loginnav'),
+    register = $('#registernav'),
+    loginfrom = $('#login'),
+    registerfrom = $('#register'),
+    forget = $('#forgetpass');
+
+
+var showregister = function(){
+    login.removeClass('active');
+    register.addClass('active');
+    loginfrom.addClass('hide');
+    registerfrom.removeClass('hide');
+}
+var showlogin = function(){
+    register.removeClass('active');
+    login.addClass('active');
+    registerfrom.addClass('hide');
+    loginfrom.removeClass('hide');
+}
+register.on('click',showregister);
+login.on('click',showlogin);
+forget.on('click',showregister);
+time.on()
+
+/*
+计时器方法
+*/
+var x = 5;
+time.on('click', timedown);
+function timedown() {
+  if (x > 0) {
+    x--;
+    setTimeout("timedown()", 1000);
+    times.innerHTML = "&nbsp" + x + "<span style='color:#FF3030'> &nbsp秒后可重发</span>";
+    // times.innerHTML = "<span></span>"
+    time.addClass('hide');
+  }else {
+    x=5;
+    times.innerHTML = " ";
+    time.removeClass('hide');
+  }
+}
